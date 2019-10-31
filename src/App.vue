aa0f2e0100e9a68079759560fc487240059c20e0
Nathaniel Inman <nate@theoestudio.com>
2019-10-31 13:59:13 -0500

    finished basic boilerplate using trufactor lib + esri

---
+++ src/App.vue
@@ -0,0 +1,132 @@
+<template lang="pug">
+#map
+</template>
+<script>
+import {Trufactor} from '@trufactor/core';
+import {loadModules as loadESRI} from 'esri-loader';
+
+export default {
+  name: 'app',
+  data(){
+    return {
+      db: new Trufactor({domain: 'demo'}),
+      layer: {
+        title: 'TruFactor Boilerplate',
+        copyright: 'TruFactor',
+        renderer: {
+          type: 'heatmap',
+          colorStops: [
+            { color: "rgba(63, 40, 102, 0)", ratio: 0 },
+            { color: "#472b77", ratio: 0.083 },
+            { color: "#4e2d87", ratio: 0.166 },
+            { color: "#563098", ratio: 0.249 },
+            { color: "#5d32a8", ratio: 0.332 },
+            { color: "#6735be", ratio: 0.415 },
+            { color: "#7139d4", ratio: 0.498 },
+            { color: "#7b3ce9", ratio: 0.581 },
+            { color: "#853fff", ratio: 0.664 },
+            { color: "#a46fbf", ratio: 0.747 },
+            { color: "#c29f80", ratio: 0.83 },
+            { color: "#e0cf40", ratio: 0.913 },
+            { color: "#ffff00", ratio: 1 }
+          ],
+          maxPixelIntensity: 25,
+          minPixelIntensity: 0
+        }
+      },
+      options: {
+        zoom: 3,
+        container: 'map',
+        center: [-98.5, 39.8]
+      }
+    };
+  },
+  mounted(){
+
+    // unfortunately as of Oct 31, 2019 ArcGIS still uses antiquated module
+    // types instead of ES2015 imports.
+    loadESRI([
+      'esri/Map',
+      'esri/layers/GeoJSONLayer',
+      'esri/views/MapView',
+      'esri/Graphic'
+    ])
+      .then(([Map, GeoJSONLayer, MapView, Graphic])=>{
+        const map = new Map({basemap: 'gray'}),
+              view = new MapView({map,...this.options});
+
+        // this is a lifecycle hook called after the api caches the returned
+        // data on the client
+        this.db.afterGetData = ({data})=>{
+          view.graphics.addMany(
+            data.features.map(feature=>{
+              return new Graphic({
+                geometry: {
+                  type: 'polygon',
+                  rings: feature.geometry.coordinates[0]
+                },
+                attributes: {
+                  ObjectId: feature.properties.index,
+                  date: feature.properties.date,
+                  total: feature.properties.total
+                },
+                symbol: {
+                  type: "simple-fill", // autocasts as new SimpleFillSymbol()
+                  color: [227, 139, 79, 0.3],
+                  outline: {
+                    // autocasts as new SimpleLineSymbol()
+                    color: [255, 255, 255],
+                    width: 1
+                  }
+                }
+              });
+            })
+          );
+        };
+
+        // go ahead and initialize the first api data call
+        this.db.getData({
+          zoom: view.zoom/22, //normalize level between 0 & 1
+          query: view.center
+        });
+
+      })
+      .catch(err=>{
+
+        // ensure we catch any errors if the ESRI modules change and become inconsistent
+        console.error(err);
+      });
+    /*
+    this.map = new mapboxgl.Map(this.options); //initialize map
+
+    // this lifecycle hooks is called when mapbox is loaded and ready
+    this.map.on('load',()=>{
+      this.map.addSource('trufactor-data',this.trufactorData);
+      this.map.addLayer(this.heatmapLayer);
+
+      // this is a lifecycle hook called after the api caches the returned
+      // data on the client
+      this.db.afterGetData = ({data})=>{
+
+        // update the maps data
+        this.trufactorData.data.features = data.features||[];
+
+        // update mapbox to reflect the data change
+        this.map.getSource('trufactor-data').setData(this.trufactorData.data);
+
+      };
+
+      // go ahead and initialize the first api data call
+      this.db.getData({
+        zoom: this.map.getZoom()/22, //normalize level between 0 & 1
+        query: this.map.getBounds()
+      });
+    });*/
+  }
+};
+</script>
+<style lang="stylus" scoped>
+#map
+  height 100vh
+  width 100vw
+</style>
